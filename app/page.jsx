aba284166aa1815ced0468e2dbe93164b91724a1
Ziad Abou-Ghali <ziadaboughali@gmail.com>
2024-07-30 22:19:08 -0400

    github / linkedin icon proper routing

--- app/page.jsx
+++ app/page.jsx
@@ -84,7 +84,7 @@ const Home = () => {
                 </div>
               </div>
               {/* Button to Visualized Resume */}
-              <Link href="#resume" passHref>
+              <Link href="resume" passHref>
                 <Button variant="primary" size="lg" className="bg-red-500 text-white hover:bg-red-700">
                   Go to Resume Visualization
                 </Button>
